8cb38c3dbe0f3c8fcc99107487a2d3e10b35d86d
AleksTereh <aleks.tereh@yandex.ru>
2022-03-12 23:29:27 +0300

    начало кода, присваеваем кнопки

--- app/src/main/java/ru/alekstereh/trafficlights/MainActivity.java
+++ app/src/main/java/ru/alekstereh/trafficlights/MainActivity.java
@@ -2,13 +2,37 @@ package ru.alekstereh.trafficlights;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean start_stop = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout b_1 = findViewById(R.id.red);
+        LinearLayout b_2 = findViewById(R.id.yellow);
+        LinearLayout b_3 = findViewById(R.id.green);
+
+    }
+
+    public void clickStart(View view) {
+
+        new Thread(() -> {
+            while (start_stop) {
+                try{
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
     }
 }
