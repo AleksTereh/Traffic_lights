ab8f605a1b04358ebc2056f92f6411fd49f2af28
AleksTereh <aleks.tereh@yandex.ru>
2022-03-13 17:23:50 +0300

    готово

--- app/src/main/java/ru/alekstereh/trafficlights/MainActivity.java
+++ app/src/main/java/ru/alekstereh/trafficlights/MainActivity.java
@@ -1,38 +1,69 @@
 package ru.alekstereh.trafficlights;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean start_stop = true;
+    private LinearLayout b_1, b_2, b_3;
+    private boolean start_stop = false;
+    private Button btn;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout b_1 = findViewById(R.id.red);
-        LinearLayout b_2 = findViewById(R.id.yellow);
-        LinearLayout b_3 = findViewById(R.id.green);
+        b_1 = findViewById(R.id.red);
+        b_2 = findViewById(R.id.yellow);
+        b_3 = findViewById(R.id.green);
+        btn = findViewById(R.id.button);
 
     }
 
     public void clickStart(View view) {
 
-        new Thread(() -> {
-            while (start_stop) {
-                try{
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        if (!start_stop) {
+            start_stop = true;
+            btn.setText("СТОП");
 
-            }
-        }).start();
+            new Thread(() -> {
+                while (start_stop) {
+                    counter++;
+                    switch (counter) {
+                        case 1:
+                            b_1.setBackgroundColor(getResources().getColor(R.color.red1));
+                            b_2.setBackgroundColor(getResources().getColor(R.color.gray));
+                            b_3.setBackgroundColor(getResources().getColor(R.color.gray));
+                            break;
+                        case 2:
+                            b_1.setBackgroundColor(getResources().getColor(R.color.gray));
+                            b_2.setBackgroundColor(getResources().getColor(R.color.yellow1));
+                            b_3.setBackgroundColor(getResources().getColor(R.color.gray));
+                            break;
+                        case 3:
+                            b_1.setBackgroundColor(getResources().getColor(R.color.gray));
+                            b_2.setBackgroundColor(getResources().getColor(R.color.gray));
+                            b_3.setBackgroundColor(getResources().getColor(R.color.green1));
+                            counter = 0;
+                            break;
+                    }
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
+        } else {
+            start_stop = false;
+            btn.setText("СТАРТ");
+        }
     }
 }
